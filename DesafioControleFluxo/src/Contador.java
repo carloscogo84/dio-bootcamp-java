e6d461d822ec6e68d969926e9050d755655a6424
carloscogo84 <carloscogo84@gmail.com>
2024-05-13 22:43:59 +0100

    Add desafio controle fluxo

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) throws Exception {
+
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parâmetro: ");
+        int parametroUm = terminal.nextInt();
+
+        System.out.println("Digite o segundo parâmetro: ");
+        int parametroDois = terminal.nextInt();
+
+        try{
+            contar(parametroUm, parametroDois);
+        }
+        catch(ParametrosInvalidosException error){
+            System.out.println(error.getMessage());
+        }
+        finally{
+            terminal.close();
+        }
+
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
+        if(parametroUm > parametroDois){
+            throw new ParametrosInvalidosException("O segundo parâmentro deve ser maior que o primeiro");
+        }
+        int contagem = parametroDois - parametroUm;
+        for(int i=0; i<=contagem; i++){
+            System.out.println("Imprimindo o número: " + i);
+        }
+    }
+    
+}
+
