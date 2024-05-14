42250662a3836cb9b4a54e25239854430699cdd8
carloscogo84 <carloscogo84@gmail.com>
2024-05-14 08:36:04 +0100

    Alterado o laço for

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -28,7 +28,7 @@ public class Contador {
             throw new ParametrosInvalidosException("O segundo parâmentro deve ser maior que o primeiro");
         }
         int contagem = parametroDois - parametroUm;
-        for(int i=0; i<=contagem; i++){
+        for(int i=1; i<=contagem; i++){
             System.out.println("Imprimindo o número: " + i);
         }
     }
